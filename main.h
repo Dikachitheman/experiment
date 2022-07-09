94ddcd46c1be8c46df4cb33f24903fc668dfc875
Chukwujama <Chukwujama>
2022-07-09 16:04:06 -0700

    Add

--- main.h
+++ main.h
@@ -2,9 +2,22 @@
 #define _MAIN_H_
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
+#include <stdlib.h> /* malloc, free */
+#include <stdarg.h> /* va_arg */
+#include <unistd.h> /* write */
 
+/**
+ * struct types - struct
+ * @id: identifier of type to print (e.g. c means char)
+ * @func: pointer to functions that print according to identifier (e.g. print_c)
+ */
+typedef struct types
+{
+	char id;
+	char* (*func)(va_list);
+} print;
+
+int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
