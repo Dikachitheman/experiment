78341a3cecbdf2468d0a6d2d952ba87577879a97
Chukwujama <Chukwujama>
2022-07-09 08:08:17 -0700

    Add

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h> /* write */
+#include <stdlib.h> /* malloc, free */
+#include <stdarg.h> /* va_arg */
+
+int _printf(const char *format, ...);
+
+#endif
