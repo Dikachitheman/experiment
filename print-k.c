97284acc65b2ae67b26844a650373f9e329c3a53
Dikachitheman <104804948+Dikachitheman@users.noreply.github.com>
2022-07-09 15:43:55 +0100

    Delete print-k.c
    cc

--- print-k.c
+++
@@ -1,8 +0,0 @@
-/**
- * steps to include. 
- * you ar allowed to copy this file as make changes to any section including this comment section.
- * im legit just brainstorming
- * 
- * 
- * the printk 
- */
