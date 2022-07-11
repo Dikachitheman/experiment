f11eccfd4730a291d983c6ae4d473f629fd69f44
Chukwujama <Chukwujama>
2022-07-11 10:59:34 -0700

    Add

--- tests/testerint.c
+++ tests/testerint.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -47,16 +48,15 @@ int print_int(va_list args)
 	}
 
 	for (i = 0; n / divisor > 9; i++, divisor *= 10);
-	
+
 	for (; divisor >= 1; n %= divisor, divisor /= 10, c++)
 	{
 		resp = n / divisor;
 		putchar('0' + resp);
-		
 	}
 
 	return (c);
-	
+
 }
 
 int print_char(va_list args)
@@ -120,20 +120,21 @@ int printk(const char *format, ...)
 
 int main(void)
 {
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
+	int len;
+	int len2;
+	unsigned int ui __attribute__((unused));
+	void *addr __attribute__((unused));
 
-    len = printk("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
-    // printk("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    printk("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-
+	len = printk("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	ui = (unsigned int)INT_MAX + 1024;
+	addr = (void *)0x7ffe637541f0;
+	printk("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	printk("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	printk("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
 
         return (0);
 }
