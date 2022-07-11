09d51bf3dc4073f8fb0a3f33c3cd9be2dcc5b19c
dikachitheman <dikachi.anosike@gmail.com>
2022-07-11 15:02:04 +0100

    nnnn

--- spec.c
+++ spec.c
@@ -1,64 +1,32 @@
-#include "main.h"
-
-int (*get_func)(va_list, char c)
+/**
+ * get_func - select function for conversion char
+ * @c: char to check
+ * Return: pointer to function
+ */
+int (*get_func(const char c))(va_list)
 {
-	specptr functs[] = 
-	{
-		{'c', print_char},
-		{'s', print_str},
-		{'d', print_int},
-		{'i', print_int},
-		{'u', print_unsigned_number},
-		{'o', print_octal},
-		{'x', print_hex},
-		{'X', print_HEX},
-		{'R', print_rot13},
-		{'b', print_binary},
-		// {'S', print_Str}
+	int i = 0;
+
+	print_spec ps[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"d", print_int},
+		{"s", print_str},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"u", print_unsigned},
+		{"S", print_unprintable_str},
+		{"r", print_reverse},
+		{"p", print_address},
+		{"R", print_rot13},
+		{"%", print_percent}
 	};
+	int specs = 14;
 
-	int i;
-
-	for (i = 0; functs[i].id; i++)
-	{
-		if (c == functs[i].id)
-		{
-			return (functs[i].fnspec);
-		}
-	}
-
+	for (i = 0; i < specs; i++)
+		if (ps[i].id == c)
+			return (ps[i].func);
 	return (NULL);
 }
-
-
-
-
-
-
-// int i = 0, j = 0, first_index;
-
-// 	first_index = index;
-// 	while (functs[i].id)
-// 	{
-// 		if (functs[index] == functs[i].id[j])
-// 		{
-// 			if (functs[i].id[j + 1] != '\0')
-// 				index++, j++;
-// 			else
-// 				break;
-// 		}
-// 		else
-// 		{
-// 			j = 0;
-// 			i++;
-// 			index = first_index;
-// 		}
-// 	}
-// 	return (functs[i].fnspec);
-
-
-// typedef struct print_spec
-// {
-// 	char *id;
-// 	int (*fnspec)(va_list);
-// }specptr;
